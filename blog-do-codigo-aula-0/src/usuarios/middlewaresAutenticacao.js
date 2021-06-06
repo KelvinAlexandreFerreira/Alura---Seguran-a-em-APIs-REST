f295fc6ce7975baf58a018f2d43b163a93c3bfa8
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-06-06 17:09:47 -0400

    Verificando refresh tokens

--- blog-do-codigo-aula-0/src/usuarios/middlewaresAutenticacao.js
+++ blog-do-codigo-aula-0/src/usuarios/middlewaresAutenticacao.js
@@ -1,11 +1,28 @@
-const { authenticate } = require('passport');
 const passport = require('passport');
+const Usuario = require('./usuarios-modelo');
+const { InvalidArgumentError } = require('../erros');
+const allowlistRefreshToken = require('../../redis/allowlist-refresh-token');
+
+async function verificaRefreshToken(refreshToken) {
+    if (!refreshToken) {
+        throw new InvalidArgumentError('Refresh Token não enviado');
+    }
+    const id = await allowlistRefreshToken.buscaValor(refreshToken);
+    if (!id) {
+        throw new InvalidArgumentError('Refresh Token inválido');
+    }
+    return id;
+}
+
+async function invalidaRefreshToken(refreshToken) {
+    await allowlistRefreshToken.deleta(refreshToken);
+}
 
 module.exports = {
-    local: (req, res, next) => {
+    local(req, res, next) {
         passport.authenticate(
-            'local', 
-            { session:false }, 
+            'local',
+            { session: false },
             (erro, usuario, info) => {
                 if (erro && erro.name === 'InvalidArgumentError') {
                     return res.status(401).json({ erro: erro.message });
@@ -25,7 +42,7 @@ module.exports = {
         )(req, res, next);
     },
 
-    bearer: (req, res, next) => {
+    bearer(req, res, next) {
         passport.authenticate(
             'bearer',
             { session: false },
@@ -51,5 +68,20 @@ module.exports = {
                 return next();
             }
         )(req, res, next);
+    },
+
+    async refresh(req, res, next) {
+        try {
+            const { refreshToken } = req.body;
+            const id = await verificaRefreshToken(refreshToken);
+            await invalidaRefreshToken(refreshToken);
+            req.user = await Usuario.buscaPorId(id);
+            return next();
+        } catch (erro) {
+            if (erro.name = InvalidArgumentError) {
+                res.status(401).json({ erro: erro.message });
+            }
+            return res.status(500).json({ erro: erro.message })
+        }
     }
 };
